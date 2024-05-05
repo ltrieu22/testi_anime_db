99bcf3a76661dfc37e98ec7844e538ca9ed3cf39
Luan Trieu <Luan.Trieu@student.oulu.fi>
2024-05-05 15:05:06 +0300

    Updated data.json, fixed anime_data_updater.py to properly work with UTF-8

--- anime_data_updater.py
+++ anime_data_updater.py
@@ -2,26 +2,23 @@ import os
 import json
 
 def main():
-    # Get the directory of the script
     script_dir = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(script_dir, 'data.json')
 
-    # Load existing data from data.json
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             existing_data = json.load(file)
     except FileNotFoundError:
         print(f"Error: '{file_path}' file not found.")
         return
-
-    # Automatically assign the ID as one more than the last entry's ID
+    
     if existing_data:
         last_entry_id = existing_data[-1]['id']
     else:
         last_entry_id = 0
     new_entry_id = last_entry_id + 1
 
-    # Ask the user 6 questions and store the answers in a dictionary
+    # ask the user 6 questions and store the answers in a dictionary
     user_data = {}
     user_data['id'] = new_entry_id
     user_data['anime_name'] = input("Enter Anime Name: ")
@@ -31,11 +28,9 @@ def main():
     user_data['description'] = input("Enter Description: ")
     user_data['osu'] = input("Enter OSU: ")
 
-    # Append the user data to the existing list
     existing_data.append(user_data)
 
-    # Write the updated data back to data.json
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(existing_data, file, indent=4)
 
     print("Data appended successfully.")
